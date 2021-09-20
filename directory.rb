28d029d07c9611b990a9396535f4ae48d29ac4a6
parsa <parsamesg@gmail.com>
2021-09-20 13:34:54 +0100

    store students in array of hashes

--- directory.rb
+++ directory.rb
@@ -1,31 +1,32 @@
-# let's put all students into an array
+# let's put all students into an array of hashes
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(names)
-  # loops through array passed in, printing out each item
-  names.each do |name|
-    puts name
+def print(students)
+  # loops through array passed in, printing out each name and cohort in the hash
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "\nOverall, we have #{students.count} great students"
 end
 
 print_header
