68167bb7116d38ac66f72ea4f001cf0ca5611ab1
parsa <parsamesg@gmail.com>
2021-09-20 13:24:43 +0100

    create methods for common procedures

--- directory.rb
+++ directory.rb
@@ -12,14 +12,22 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-
-puts "The students of Villains Academy"
-puts "-------------"
-# and then loop through the array, printing each one
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
+def print(names)
+  # loops through array passed in, printing out each item
+  names.each do |name|
+    puts name
+  end
+end
 
-# finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
